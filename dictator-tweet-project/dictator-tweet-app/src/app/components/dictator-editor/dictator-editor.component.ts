5e7c609c635f905a41fc05546be460b63aeba058
Jonas Hansen <jonash871j@gmail.com>
2022-05-11 19:27:14 +0200

    Minor change

--- dictator-tweet-project/dictator-tweet-app/src/app/components/dictator-editor/dictator-editor.component.ts
+++ dictator-tweet-project/dictator-tweet-app/src/app/components/dictator-editor/dictator-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dictator-editor',
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class DictatorEditorComponent implements OnInit {
 
   constructor() { }
+  @Input() isEditMode : boolean = false;
 
   ngOnInit(): void {
   }
-
 }
